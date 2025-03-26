44a5179a68e66359254f81975edb2880db7ce660
Артур Лагунов <a.lagunov@ktsstudio.ru>
2025-03-26 18:36:45 +0400

    1

--- svc/api/mg_api/infra/sio/sid_registry.py
+++ svc/api/mg_api/infra/sio/sid_registry.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from typing import Iterator, Tuple
-
+from uuid import UUID
 from attrs import define, field
 
 
@@ -8,11 +8,11 @@ from attrs import define, field
 class SidRegistry:
     _user_sid: dict = field(factory=lambda: defaultdict(set), init=False)
 
-    def __setattr__(self, key: str, value: str) -> None:
+    def __setattr__(self, key: UUID, value: str) -> None:
         self._user_sid[key].add(value)
 
     def __getattr__(self, key: str) -> str:
         return self._user_sid[key]
 
-    def __iter__(self) -> Iterator[Tuple[str, set[str]]]:
+    def __iter__(self) -> Iterator[Tuple[UUID, set[str]]]:
         return iter(self._user_sid.items())
