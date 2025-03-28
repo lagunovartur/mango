c1e3978a0f752093756df0c324fefc7b2db26270
Артур Лагунов <a.lagunov@ktsstudio.ru>
2025-03-28 17:27:01 +0400

    1

--- svc/api/mg_api/utils/crud/list_svc.py
+++ svc/api/mg_api/utils/crud/list_svc.py
@@ -76,5 +76,3 @@ class ListSvc(IListSvc, Generic[R, M, LP]):
     async def _paginate(self, pagination: PageParams) -> None:
         self._stmt = self._stmt.limit(pagination.limit).offset(pagination.offset)
 
-
-LS = TypeVar("LS", bound=ListSvc)
